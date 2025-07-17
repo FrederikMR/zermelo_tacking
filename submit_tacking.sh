5770bfc7279ede9ef8832e5855b897c7fa6d5060
Frederik-DTU <s164222@student.dtu.dk>
2025-07-17 12:56:28 +0200

    Update

--- submit_tacking.sh
+++ submit_tacking.sh
@@ -1,6 +1,6 @@
     #! /bin/bash
     #BSUB -q gpua100
-    #BSUB -J time_position_fixed
+    #BSUB -J poincarre_north_a_fixed
     #BSUB -n 4
     #BSUB -gpu "num=1:mode=exclusive_process"
     #BSUB -W 24:00
@@ -17,14 +17,14 @@
     module swap python3/3.10.12
     
     python3 tacking.py \
-        --manifold time_position \
+        --manifold poincarre_north_a \
         --geometry fixed \
         --method adam \
         --T 1000 \
         --lr_rate 0.01 \
         --alpha 1.0 \
         --tol 0.0001 \
-        --max_iter 1000 \
+        --max_iter 10000 \
         --sub_iter 10 \
         --N_sim 5 \
         --idx_birds 0 \
