d25c61f75c38081f66b306a38c00666c6aabdb1b
Frederik Mbius Rygaard <fmry@pop-os.localdomain>
2025-08-09 17:28:03 +0200

    Update

--- geometry/manifolds/time_only.py
+++ geometry/manifolds/time_only.py
@@ -30,15 +30,50 @@ class TimeOnly(LorentzFinslerManifold):
     def F_metric(self, t, x, v):
         
         x,y = v[0], v[1]
-        Pi = jnp.pi
-        sin, cos = jnp.sin, jnp.cos
+        
+        sin_t = jnp.sin(t)
+        cos_t_pi4 = jnp.cos(t * jnp.pi / 4)
+        sin_t_pi4 = jnp.sin(t * jnp.pi / 4)
     
-        f = ((1.0*(2+1/2*sin(t))**2)*sin(1/4*t*jnp.pi)*y+1.0*((3/2+3/8*sin(t))**2)*cos(1/4*t*jnp.pi)*x \
-             +(((2+1/2*sin(t))**4)*((3/2+3/8*sin(t))**2)*(y**2)+((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**4)*(x**2) \
-               -1.00*((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)*(cos(1/4*t*Pi)**2)*(y**2) \
-               +2.00*((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)*cos(1/4*t*Pi)*sin(1/4*t*Pi)*x*y \
-               -1.00*(2+1/2*(sin(t))**2)*((3/2+3/8*sin(t))**2)*(sin(1/4*t*Pi)**2)*(x**2))**(1/2)) \
-        /(((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)-1.00*((2+1/2*sin(t))**2)*(sin(1/4*t*Pi)**2) \
-          -1.00*((3/2+3/8*sin(t))**2)*(cos(1/4*t*Pi)**2))
+        a = (2 + sin_t / 2)         # Common factor A
+        b = (3/2 + (3 * sin_t) / 8) # Common factor B
+        a2 = a**2
+        b2 = b**2
+        a4 = a**4
+        b4 = b**4
     
-        return f
+        # Numerator
+        term1 = a2 * sin_t_pi4 * y
+        term2 = b2 * cos_t_pi4 * x
+    
+        sqrt_inner = (
+            a4 * b2 * y**2 +
+            a2 * b4 * x**2 -
+            a2 * b2 * cos_t_pi4**2 * y**2 +
+            2 * a2 * b2 * cos_t_pi4 * sin_t_pi4 * x * y -
+            a2 * b2 * sin_t_pi4**2 * x**2
+        )
+    
+        numerator = term1 + term2 + jnp.sqrt(sqrt_inner)
+    
+        # Denominator
+        denominator = (
+            a2 * b2 -
+            a2 * sin_t_pi4**2 -
+            b2 * cos_t_pi4**2
+        )
+    
+        return numerator / denominator
+        
+        #Pi = jnp.pi
+        #sin, cos = jnp.sin, jnp.cos
+    
+        #f = ((1.0*(2+1/2*sin(t))**2)*sin(1/4*t*jnp.pi)*y+1.0*((3/2+3/8*sin(t))**2)*cos(1/4*t*jnp.pi)*x \
+        #     +(((2+1/2*sin(t))**4)*((3/2+3/8*sin(t))**2)*(y**2)+((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**4)*(x**2) \
+        #       -1.00*((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)*(cos(1/4*t*Pi)**2)*(y**2) \
+        #       +2.00*((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)*cos(1/4*t*Pi)*sin(1/4*t*Pi)*x*y \
+        #       -1.00*(2+1/2*(sin(t))**2)*((3/2+3/8*sin(t))**2)*(sin(1/4*t*Pi)**2)*(x**2))**(1/2)) \
+        #/(((2+1/2*sin(t))**2)*((3/2+3/8*sin(t))**2)-1.00*((2+1/2*sin(t))**2)*(sin(1/4*t*Pi)**2) \
+        #  -1.00*((3/2+3/8*sin(t))**2)*(cos(1/4*t*Pi)**2))
+    
+        #return f
